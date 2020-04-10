c187586593707ce0365da5b72695f73a53f5e347
sachin jogchand <sachincodingkart@gmail.com>
2020-04-10 20:30:26 +0530

    initail commit

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/json-write',(req, res) => {
  var rawdata = fs.readFileSync('./json_files/new-json.json');	
   console.log("hello");
 
-  res.send(rawdata);
+  // res.send(rawdata);
 		 if(rawdata !=''){
            var file_data = JSON.parse(rawdata); 
 		   file_data.shop_name = shop_data;
